c8e32a0fbeb4e734547f63ffb708bbeaa4ab227b
FavourtheDev <favourthedeveloper@gmail.com>
2023-08-21 17:10:07 +0100

    MongoDB Connect and Interaction

--- app.js
+++ app.js
@@ -1,39 +1,58 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
 
-app.set('view engine', 'ejs');
+const Blog = require("./models/blog");
+
+const app = express();
+
+const dbURI =
+  "mongodb+srv://FavourtheDev:FavourtheDev@blogs.xj33yyr.mongodb.net/?retryWrites=true&w=majority";
+mongoose
+  .connect(dbURI)
+  .then((result) => {
+    console.log("Connected to the db");
+    app.listen(3000);
+  })
+
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.set("view engine", "ejs");
 
 app.use(express.static("./"));
 
-app.get('/', function (req, res) {
-    const blogs = [
-        {
-            title : "Post 1 Headline",
-            text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore, voluptatibus!",
-            imgsrc: 'views/blog.jpg',
-            imgalt: 'blog'
-        },
-        {
-            title : "Post 1 Headline",
-            text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore, voluptatibus!",
-            imgsrc: 'views/blog1.jpg',
-            imgalt: 'blog1'
-        },
-        {
-            title : "Post 1 Headline",
-            text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore, voluptatibus!",
-            imgsrc: 'views/blog2.jpg',
-            imgalt: 'blog2'
-        },
-       
-    ]
-  res.render('index', {blogs, title: 'Home'});
-})
+app.use(morgan("dev"));
 
+app.get("/", function (req, res) {
+  Blog.find()
+  .then((result) => {
+    res.render("index", { blogs : result, title: "Home" });
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+});
 
-app.get('/new', function (req, res) {
-  res.render('style');
-})
+app.get("/new", function (req, res) {
+  res.render("style");
+});
 
+app.get("/saveblogs", (req, res) => {
+  const blog = new Blog({
+    title: "Post 5 Headlineeee",
+    text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore, voluptatibus!",
+    imgsrc: "views/blog1.jpg",
+    imgalt: "blog1",
+  });
 
-app.listen(3000);
+  blog.save()
+    .then((result) => {
+      res.send(result);
+    })
+
+    .catch((err) => {
+      console.log(err);
+    })
+});
---
+++ models/blog.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const blogSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    imgsrc: {
+        type: String,
+        required: true
+    },
+    imgalt: {
+        type: String,
+        required: true
+    }
+}, { timestamps:true });
+
+const Blog = mongoose.model('Blog', blogSchema);
+
+module.exports = Blog;
