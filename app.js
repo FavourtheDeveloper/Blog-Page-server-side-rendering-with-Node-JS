6685e5e5048f90aa3478ada5ddbc91267a3d627e
FavourtheDev <favourthedeveloper@gmail.com>
2023-08-22 14:42:11 +0100

    Added CRUD

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ mongoose
 app.set("view engine", "ejs");
 
 app.use(express.static("./"));
+app.use(express.urlencoded({ extended: true }))
 
 app.use(morgan("dev"));
 
@@ -34,6 +35,18 @@ app.get("/", function (req, res) {
     console.log(err);
   })
 });
+
+
+app.post("/", (req, res) => {
+  const blog = new Blog(req.body);
+  blog.save()
+  .then((result) => {
+    res.redirect('/'); 
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+})
 
 app.get("/new", function (req, res) {
   res.render("style");
@@ -56,3 +69,27 @@ app.get("/saveblogs", (req, res) => {
       console.log(err);
     })
 });
+
+app.get('/index/:id', (req, res) => {
+  const id = req.params.id;
+  Blog.findById(id)
+  .then((result) => {
+    res.render('details', { blog: result, title: "Blog Details" });
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+
+
+})
+
+app.delete('/index/:id', (req, res) => {
+  const id = req.params.id;
+  Blog.findByIdAndDelete(id)
+  .then((result) => {
+    res.json({ redirect: '/' });
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+})
